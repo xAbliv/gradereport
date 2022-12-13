ea4e95ccb5d6579c38a018e3532b567ec70e3304
xAbliv <99667142+xAbliv@users.noreply.github.com>
2022-12-13 08:08:11 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function pinInput(valNum) {
+  valNum = parseFloat(valNum);
+  
+  if (valNum === 6969){
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("main").style.display = "block";
+  }
+}
