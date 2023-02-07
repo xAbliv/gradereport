c159f4ad97d2c78947d61556d9eeacf1a9c62d6c
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-02-07 14:28:19 -0500

    ..

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const input = document.getElementById("input");
 const output = document.getElementById("output");
 
 input.addEventListener("input", function () {
+  inputnonhash = parseFloat(value);
   const hash = CryptoJS.SHA256(input.value).toString();
   alert(hash);
   if (hash === "350C94D619F6ABA3379500FF11BFCCA6E58B0AFE5B3624D0AD56FA607845E38C") {
