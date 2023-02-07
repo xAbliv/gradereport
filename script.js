5cd350e21ef25a325d0a578b7d31d98b1f6f9ae4
xAbliv <99667142+xAbliv@users.noreply.github.com>
2023-02-07 10:56:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function pinInput(valNum) {
   valNum = parseFloat(valNum);
   
-  if (valNum === 6969){
+  if (valNum === 8347){
     document.getElementById("s1").style.display = "none";
     document.getElementById("main").style.display = "block";
   }
