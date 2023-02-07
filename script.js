d81a3f1811daf13ac1431b71c89441cf38d40244
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-02-07 13:27:11 -0500

    Hashing

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function pinInput(valNum) {
   valNum = parseFloat(valNum);
-  
-  if (valNum === 8347){
+
+  if (valNum === 8347) {
     document.getElementById("s1").style.display = "none";
     document.getElementById("main").style.display = "block";
   }
 }
+
+//dont touch
+
+const textbox = document.getElementById("input");
+const hash = new TextEncoder().encode(textbox.value);
+const hashArray = await crypto.subtle.digest("SHA-256", hash);
+const hashHex = Array.from(new Uint8Array(hashArray))
+  .map(b => b.toString(16).padStart(2, "0"))
+  .join("");
+console.log(hashHex);
