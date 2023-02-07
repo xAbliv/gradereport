ed3b0a3e49b20606b1b3977214017142cebfe8a3
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-02-07 14:22:43 -0500

    ...

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-const textbox = document.getElementById("input");
-const hash = new TextEncoder().encode(textbox.value);
-const hashArray = await crypto.subtle.digest("SHA-256", hash);
-const hashHex = Array.from(new Uint8Array(hashArray))
+// function pinInput(valNum) {
+//   valNum = parseFloat(valNum);
 
-function pinInput(valNum) {
-  valNum = parseFloat(valNum);
+//   if (valNum === 8347) {
+//     document.getElementById("s1").style.display = "none";
+//     document.getElementById("main").style.display = "block";
+//   }
+// }
 
-  if (valNum === 8347) {
-    document.getElementById("s1").style.display = "none";
-    document.getElementById("main").style.display = "block";
+//dont touch anything below this line
+
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+
+input.addEventListener("input", function () {
+  const hash = CryptoJS.SHA256(input.value).toString();
+  output.innerHTML = hash;
+  if (hash === "350C94D619F6ABA3379500FF11BFCCA6E58B0AFE5B3624D0AD56FA607845E38C") {
+    runFunction();
   }
+});
+
+function runFunction() {
+  document.getElementById("s1").style.display = "none";
+  document.getElementById("main").style.display = "block";
 }
 
-//dont touch
-
-const textbox = document.getElementById("input");
-const hash = new TextEncoder().encode(textbox.value);
-const hashArray = await crypto.subtle.digest("SHA-256", hash);
-const hashHex = Array.from(new Uint8Array(hashArray))
-  .map(b => b.toString(16).padStart(2, "0"))
-  .join("");
-console.log(hashHex);
-
-function pinInputHashed(valNumHash) {
-  valNumHash = parseFloat(valNumHash)
-
-  if (valNumHash === 'F0F06E6A36F7CB803C295C948C8597932FE6854DBAF3BB81945CE4ADB9724C7A') {
-    document.getElementById("s1").style.display = 'none';
-    document.getElementById("main").style.display = 'block';
-  }
-}
+// oninput="pinInput(this.value)" onchange="pinInput(this.value)"
