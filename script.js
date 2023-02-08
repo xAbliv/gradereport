9cd002767265f6cd78c40aab8aa91109cef5e853
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-02-07 19:36:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const textInput = document.getElementById("textInput");
+var textInput = document.getElementById("textInput");
 
 function updateHash() {
-    const input = textInput.value;
-    const hash = CryptoJS.SHA256(input).toString();
+    var input = textInput.value;
+    var hash = CryptoJS.SHA256(input).toString();
     if (hash === "350c94d619f6aba3379500ff11bfcca6e58b0afe5b3624d0ad56fa607845e38c") {
         document.getElementById("s1").style.display = "none";
         document.getElementById("main").style.display = "block";
