cc65529d08fbcb2508c1d1ea7cdd60716eaba98a
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-02-08 07:57:42 -0500

    SHA3-256

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var textInput = document.getElementById("textInput");
 
 function updateHash() {
     var input = textInput.value;
-    var hash = CryptoJS.SHA256(input).toString();
-    if (hash === "350c94d619f6aba3379500ff11bfcca6e58b0afe5b3624d0ad56fa607845e38c") {
+    var hash = CryptoJS.SHA3(input).toString();
+    if (hash === "a13146a82de041bb9faa75656b88c49c515e5efe119e5edddd8e64b5cd6976b85cd1d16435a05e8c99f2c86dc52d724f7a64fecfcf55639259694d2dc050bdd6") {
         document.getElementById("s1").style.display = "none";
         document.getElementById("main").style.display = "block";
     } else {
