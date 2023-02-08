b5097ec684186d0d7b61332c346a99acbd4c19ee
xAbliv <99667142+xAbliv@users.noreply.github.com>
2023-02-08 08:13:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-var textInput = document.getElementById("textInput");
+function pinInput(valNum) {
+ valNum = parseFloat(valNum);
 
-function updateHash() {
-    var input = textInput.value;
-    var hash = CryptoJS.SHA256(input).toString();
-    if (hash === "350c94d619f6aba3379500ff11bfcca6e58b0afe5b3624d0ad56fa607845e38c") {
-        document.getElementById("s1").style.display = "none";
-        document.getElementById("main").style.display = "block";
-    } else {
-        return null;
-    }
-}
+ if (valNum === 8347) {     document.getElementById("s1").style.display = "none";
+document.getElementById("main").style.display = "block";
+   }
+ }
